b95afe625ba401ecdffb06011cb3a1975e059c1d
Mark Root-Wiley <info@mrwweb.com>
2024-03-17 20:55:10 -0700

    add new help button with documentation for buttons, shortcuts, and magic link pasting. closes #23

--- plans-editor-toolbar.user.js
+++ plans-editor-toolbar.user.js
@@ -14,20 +14,26 @@
 
 function PlansEditorToolbar() {
     const icons = {
-        bold: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M6 4v13h4.54c1.37 0 2.46-.33 3.26-1 .8-.66 1.2-1.58 1.2-2.77 0-.84-.17-1.51-.51-2.01s-.9-.85-1.67-1.03v-.09c.57-.1 1.02-.4 1.36-.9s.51-1.13.51-1.91c0-1.14-.39-1.98-1.17-2.5C12.75 4.26 11.5 4 9.78 4H6zm2.57 5.15V6.26h1.36c.73 0 1.27.11 1.61.32.34.22.51.58.51 1.07 0 .54-.16.92-.47 1.15s-.82.35-1.51.35h-1.5zm0 2.19h1.6c1.44 0 2.16.53 2.16 1.61 0 .6-.17 1.05-.51 1.34s-.86.43-1.57.43H8.57v-3.38z"/></svg>',
-        italic: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M14.78 6h-2.13l-2.8 9h2.12l-.62 2H4.6l.62-2h2.14l2.8-9H8.03l.62-2h6.75z"/></svg>',
-        link: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M17.74 2.76c1.68 1.69 1.68 4.41 0 6.1l-1.53 1.52c-1.12 1.12-2.7 1.47-4.14 1.09l2.62-2.61.76-.77.76-.76c.84-.84.84-2.2 0-3.04-.84-.85-2.2-.85-3.04 0l-.77.76-3.38 3.38c-.37-1.44-.02-3.02 1.1-4.14l1.52-1.53c1.69-1.68 4.42-1.68 6.1 0zM8.59 13.43l5.34-5.34c.42-.42.42-1.1 0-1.52-.44-.43-1.13-.39-1.53 0l-5.33 5.34c-.42.42-.42 1.1 0 1.52.44.43 1.13.39 1.52 0zm-.76 2.29l4.14-4.15c.38 1.44.03 3.02-1.09 4.14l-1.52 1.53c-1.69 1.68-4.41 1.68-6.1 0-1.68-1.68-1.68-4.42 0-6.1l1.53-1.52c1.12-1.12 2.7-1.47 4.14-1.1l-4.14 4.15c-.85.84-.85 2.2 0 3.05.84.84 2.2.84 3.04 0z"/></svg>',
+        bold: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M6 4v13h4.54c1.37 0 2.46-.33 3.26-1 .8-.66 1.2-1.58 1.2-2.77 0-.84-.17-1.51-.51-2.01s-.9-.85-1.67-1.03v-.09c.57-.1 1.02-.4 1.36-.9s.51-1.13.51-1.91c0-1.14-.39-1.98-1.17-2.5C12.75 4.26 11.5 4 9.78 4H6zm2.57 5.15V6.26h1.36c.73 0 1.27.11 1.61.32.34.22.51.58.51 1.07 0 .54-.16.92-.47 1.15s-.82.35-1.51.35h-1.5zm0 2.19h1.6c1.44 0 2.16.53 2.16 1.61 0 .6-.17 1.05-.51 1.34s-.86.43-1.57.43H8.57v-3.38z"/></svg>',
+        italic: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M14.78 6h-2.13l-2.8 9h2.12l-.62 2H4.6l.62-2h2.14l2.8-9H8.03l.62-2h6.75z"/></svg>',
+        link: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M17.74 2.76c1.68 1.69 1.68 4.41 0 6.1l-1.53 1.52c-1.12 1.12-2.7 1.47-4.14 1.09l2.62-2.61.76-.77.76-.76c.84-.84.84-2.2 0-3.04-.84-.85-2.2-.85-3.04 0l-.77.76-3.38 3.38c-.37-1.44-.02-3.02 1.1-4.14l1.52-1.53c1.69-1.68 4.42-1.68 6.1 0zM8.59 13.43l5.34-5.34c.42-.42.42-1.1 0-1.52-.44-.43-1.13-.39-1.53 0l-5.33 5.34c-.42.42-.42 1.1 0 1.52.44.43 1.13.39 1.52 0zm-.76 2.29l4.14-4.15c.38 1.44.03 3.02-1.09 4.14l-1.52 1.53c-1.69 1.68-4.41 1.68-6.1 0-1.68-1.68-1.68-4.42 0-6.1l1.53-1.52c1.12-1.12 2.7-1.47 4.14-1.1l-4.14 4.15c-.85.84-.85 2.2 0 3.05.84.84 2.2.84 3.04 0z"/></svg>',
         strike: '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 100 100"><path fill="currentColor" d="M79.168 45.832H50A15.75 15.75 0 0 1 33.332 31.25 15.751 15.751 0 0 1 50 16.668 15.75 15.75 0 0 1 66.668 31.25a4.166 4.166 0 1 0 8.332 0 24.047 24.047 0 0 0-7.77-16.617A24.063 24.063 0 0 0 50 8.332a24.061 24.061 0 0 0-17.23 6.3A24.048 24.048 0 0 0 25 31.25a21.703 21.703 0 0 0 5.75 14.582h-9.917c-2.3 0-4.164 1.867-4.164 4.168s1.863 4.168 4.164 4.168H50A15.75 15.75 0 0 1 66.669 68.75 15.751 15.751 0 0 1 50 83.332 15.75 15.75 0 0 1 33.333 68.75a4.166 4.166 0 1 0-8.332 0 24.047 24.047 0 0 0 7.77 16.617 24.063 24.063 0 0 0 17.23 6.3 24.061 24.061 0 0 0 17.23-6.3A24.048 24.048 0 0 0 75 68.75a21.703 21.703 0 0 0-5.75-14.582h9.918c2.3 0 4.164-1.867 4.164-4.168s-1.863-4.168-4.164-4.168z"/></svg>',
-        code: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M12 2l4 4v12H4V2h8zM9 13l-2-2 2-2-1-1-3 3 3 3zm3 1l3-3-3-3-1 1 2 2-2 2z"/></svg>',
-        hr: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M4 9h12v2H4V9z"/></svg>',
-        date: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M15 4h3v14H2V4h3V3c0-.83.67-1.5 1.5-1.5S8 2.17 8 3v1h4V3c0-.83.67-1.5 1.5-1.5S15 2.17 15 3v1zM6 3v2.5c0 .28.22.5.5.5s.5-.22.5-.5V3c0-.28-.22-.5-.5-.5S6 2.72 6 3zm7 0v2.5c0 .28.22.5.5.5s.5-.22.5-.5V3c0-.28-.22-.5-.5-.5s-.5.22-.5.5zm4 14V8H3v9h14zM7 16V9H5v7h2zm4 0V9H9v7h2zm4 0V9h-2v7h2z"/></svg>',
-        close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M15.75 6.75L18 3v14l-2.25-3.75L17 12h-4v4l1.25-1.25L18 17H2l3.75-2.25L7 16v-4H3l1.25 1.25L2 17V3l2.25 3.75L3 8h4V4L5.75 5.25 2 3h16l-3.75 2.25L13 4v4h4z"/></svg>',
-        save: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M14.5 2H3.7C2.7 2 2 2.7 2 3.7v12.6c0 1 .7 1.7 1.7 1.7h12.6c1 0 1.7-.7 1.7-1.7V6l-3.5-4zM10 15.6a2.1 2.1 0 1 1 0-4.2 2.1 2.1 0 0 1 0 4.2zm2.7-7.5H4.3V4.2h8.4v4z"/></svg>',
+        code: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M12 2l4 4v12H4V2h8zM9 13l-2-2 2-2-1-1-3 3 3 3zm3 1l3-3-3-3-1 1 2 2-2 2z"/></svg>',
+        hr: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M4 9h12v2H4V9z"/></svg>',
+        date: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M15 4h3v14H2V4h3V3c0-.83.67-1.5 1.5-1.5S8 2.17 8 3v1h4V3c0-.83.67-1.5 1.5-1.5S15 2.17 15 3v1zM6 3v2.5c0 .28.22.5.5.5s.5-.22.5-.5V3c0-.28-.22-.5-.5-.5S6 2.72 6 3zm7 0v2.5c0 .28.22.5.5.5s.5-.22.5-.5V3c0-.28-.22-.5-.5-.5s-.5.22-.5.5zm4 14V8H3v9h14zM7 16V9H5v7h2zm4 0V9H9v7h2zm4 0V9h-2v7h2z"/></svg>',
+        close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M15.75 6.75L18 3v14l-2.25-3.75L17 12h-4v4l1.25-1.25L18 17H2l3.75-2.25L7 16v-4H3l1.25 1.25L2 17V3l2.25 3.75L3 8h4V4L5.75 5.25 2 3h16l-3.75 2.25L13 4v4h4z"/></svg>',
+        save: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M14.5 2H3.7C2.7 2 2 2.7 2 3.7v12.6c0 1 .7 1.7 1.7 1.7h12.6c1 0 1.7-.7 1.7-1.7V6l-3.5-4zM10 15.6a2.1 2.1 0 1 1 0-4.2 2.1 2.1 0 0 1 0 4.2zm2.7-7.5H4.3V4.2h8.4v4z"/></svg>',
         planLove:
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M6 14H4V6h2V4H2v12h4M14 4v2h2v8h-2v2h4V4"/></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M6 14H4V6h2V4H2v12h4M14 4v2h2v8h-2v2h4V4"/></svg>',
+        help: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M17 10c0-3.87-3.14-7-7-7-3.87 0-7 3.13-7 7s3.13 7 7 7c3.86 0 7-3.13 7-7zm-6.3 1.48H9.14v-.43c0-.38.08-.7.24-.98s.46-.57.88-.89c.41-.29.68-.53.81-.71.14-.18.2-.39.2-.62 0-.25-.09-.44-.28-.58-.19-.13-.45-.19-.79-.19-.58 0-1.25.19-2 .57l-.64-1.28c.87-.49 1.8-.74 2.77-.74.81 0 1.45.2 1.92.58.48.39.71.91.71 1.55 0 .43-.09.8-.29 1.11-.19.32-.57.67-1.11 1.06-.38.28-.61.49-.71.63-.1.15-.15.34-.15.57v.35zm-1.47 2.74c-.18-.17-.27-.42-.27-.73 0-.33.08-.58.26-.75s.43-.25.77-.25c.32 0 .57.09.75.26s.27.42.27.74c0 .3-.09.55-.27.72-.18.18-.43.27-.75.27-.33 0-.58-.09-.76-.26z"/></svg>',
+        close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentcolor" aria-hidden="true"><path d="M14.95 6.46L11.41 10l3.54 3.54-1.41 1.41L10 11.42l-3.53 3.53-1.42-1.42L8.58 10 5.05 6.47l1.42-1.42L10 8.58l3.54-3.53z"/></svg>',
     };
     const styles = document.createElement('style');
     styles.innerHTML = `
+html:has(dialog[open]) {
+    overflow: hidden;
+}
+
 .plans-editor-toolbar {
     display: flex;
     align-items: center;
@@ -35,7 +41,8 @@ function PlansEditorToolbar() {
     margin-block-end: 0.5rem;
 }
 .plans-editor-toolbar button,
-#editbox .submitinput {
+#editbox .submitinput,
+.dialog-close {
     display: inline-flex;
     align-items: center;
     padding: .1875rem;
@@ -43,13 +50,54 @@ function PlansEditorToolbar() {
     line-height: 1;
 }
 .plans-editor-toolbar svg,
-#editbox .submitinput svg {
+#editbox .submitinput svg,
+.dialog-close svg {
     width: 20px;
     height: 20px;
 }
 .plans-editor-button--close-editor {
     display: none !important;
 }
+
+dialog {
+    width: 80vw;
+    height: 80vh;
+    padding: 40px;
+    max-width: 75ch;
+    border: 1px solid transparent;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+    
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th {
+        text-align: left;
+    }
+    th,td {
+        padding: .25em;
+    }
+    tr:nth-child(even) {
+        background: rgba(0, 0, 0, 0.05);
+    }
+    svg {
+        width: 1em;
+        height: 1em;
+    }
+}
+
+.dialog-close {
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    background: transparent;
+    border: 0;
+}
+
+.dialog-content > :first-child {
+    margin-block-start: 0;
+}
+
 @media( max-width: 40em ) {
     .plans-editor-focused {
         textarea {
@@ -83,6 +131,19 @@ function PlansEditorToolbar() {
     }
 }`;
 
+    const dialog = document.createElement('dialog');
+    const dialogContent = document.createElement('div');
+    dialogContent.classList.add('dialog-content');
+    const dialogClose = document.createElement('button');
+    dialogClose.classList.add('dialog-close');
+    dialogClose.setAttribute('aria-label', 'Close');
+    dialogClose.setAttribute('autofocus', 'true');
+    dialogClose.innerHTML = icons.close;
+    dialogClose.addEventListener('click', () => dialog.close());
+    dialog.appendChild(dialogClose);
+    dialog.appendChild(dialogContent);
+    document.body.appendChild(dialog);
+
     const editForm = document.getElementById('editbox');
     const submitButton = editForm.querySelector('.submitinput');
     const textarea = editForm.querySelector('textarea');
@@ -105,6 +166,7 @@ function PlansEditorToolbar() {
         createToolbarButton('Plan Love', 'planLove', insertPlanLove);
         createToolbarButton('[date]', 'date', insertDate);
         createToolbarButton('Horizontal Rule', 'hr', insertHr);
+        createToolbarButton('Help', 'help', toggleHelp);
         createToolbarButton('Close Editor', 'close', closeEditor);
 
         // add icon to save button and
@@ -223,7 +285,7 @@ function PlansEditorToolbar() {
      *
      * @param {string} open The opening text to wrap, e.g. <b>
      * @param {string} close The closing text to wrap, e.g. </b>
-     * @param {int} cursorOffset Number of characters after the start of the selection to place the cursor following the wrapping. Defaults to false, in which case the cursor is placed at the end of the wrapped text. If cursorPos is negative, then the cursor position is set relative to the end of the selection.
+     * @param {integer} cursorOffset Number of characters after the start of the selection to place the cursor following the wrapping. Defaults to false, in which case the cursor is placed at the end of the wrapped text. If cursorPos is negative, then the cursor position is set relative to the end of the selection.
      */
     function wrapText(open, close, cursorOffset = false) {
         // get selection
@@ -360,6 +422,88 @@ function PlansEditorToolbar() {
      */
     function isUrl(string) {
         return string.startsWith('http://') || string.startsWith('https://');
+    }
+
+    function toggleHelp() {
+        dialogContent.innerHTML = `<h2>Plans Text Editor Toolbar Help</h2>
+<h3>Buttons</h3>
+<table>
+<thead><tr>
+<th>Button</th>
+<th>Action</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>${icons.bold}</td><td>Bold*</td>
+</tr>
+<tr>
+<td>${icons.italic}</td><td>Italic*</td>
+</tr>
+<tr>
+<td>${icons.link}</td><td>Link*</td>
+</tr>
+<tr>
+<td>${icons.planLove}</td><td>Plan Love*</td>
+</tr>
+<tr>
+<td>${icons.date}</td><td>Insert Date</td>
+</tr>
+<tr>
+<td>${icons.hr}</td><td>Insert Horizontal Rule</td>
+</tr>
+<tr>
+<td>${icons.close}</td><td>Exit fullscreen mode (mobile only)</td>
+</tr>
+</tbody>
+</table>
+<p><em><small>* Button inserts formatting or wraps selected text.</small></em></p>
+<h3>Shortcuts</h3>
+<table>
+<thead>
+<tr>
+<th>Shortcut</th>
+<th>Action</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><code>Ctrl</code> + <code>b</code></td>
+<td>Bold</td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>i</code></td>
+<td>Italic</td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>k</code></td>
+<td>Make/Insert link</td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>l</code></td>
+<td>Make/Insert planlove</td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>d</code></td>
+<td>Insert <code>[date]</code></td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>h</code></td>
+<td>Insert <code>&lt;hr&gt;</code></td>
+</tr>
+<tr>
+<td><code>Ctrl</code> + <code>s</code></td>
+<td>Save plan</td>
+</tr>
+</tbody>
+</table>
+<h3>Magic Link Pasting!</h3>
+<ol>
+<li>Copy a URL</li>
+<li>Select some text</li>
+<li>*PASTE!* (<code>Ctrl</code> + <code>v</code> or Right-click > Paste)</li>
+</ol>`;
+        dialog.showModal();
     }
 }
 
